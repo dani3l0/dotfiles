b668b02055ae4ff147fb46132145fbab06af51e4
dani3l0 <daniel.jank@protonmail.com>
2026-05-30 13:20:50 +0200

    Add tuned-ppd custom configurations

---
+++ tuned-ppd/etc/tuned/profiles/custom-balanced/script.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cpupower frequency-set --min 0.40GHz
+cpupower frequency-set --max 1.80GHz
---
+++ tuned-ppd/etc/tuned/profiles/custom-performance/script.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cpupower frequency-set --min 0.80GHz
+cpupower frequency-set --max 2.20GHz
